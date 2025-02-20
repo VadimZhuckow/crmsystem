0d7471eaf853c8815781e59a0f4df36aa0678f37
Vadim Zhukov <apexknight@yandex.ru>
2025-02-20 16:14:58 +0300

    added styles

--- orders/urls/urls.py
+++ orders/urls/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from orders.views.orders import order_list, add_order
+from orders.views.orders import order_list, add_order, delete_order, update_order_status
 
 urlpatterns = [
     path('', order_list, name='order_list'),
     path('add/', add_order, name='add_order'),
-    # path('delete/<int:order_id>/', delete_order, name='delete_order'),
-    # path('update/<int:order_id>/', update_order_status, name='update_order_status'),
+    path('delete/<int:order_id>/', delete_order, name='delete_order'),
+    path('update/<int:order_id>/', update_order_status, name='update_order_status'),
 ]
--- orders/views/orders.py
+++ orders/views/orders.py
@@ -5,7 +5,7 @@ from orders.models.order import Order
 from orders.forms.orders import OrderForm
 
 def order_list(request):
-    orders = Order.objects.all()
+    orders = Order.objects.all().order_by('id')
     return render(request, 'orders/order_list.html', {'orders': orders})
 
 def add_order(request):
@@ -22,3 +22,16 @@ def add_order(request):
         except Exception as e:
             print(f"Ошибка: {e}")  
     return render(request, 'orders/add_order.html')
+
+def delete_order(request, order_id):
+    order = Order.objects.get(id=order_id)
+    order.delete()
+    return redirect('order_list')
+
+def update_order_status(request, order_id):
+    order = Order.objects.get(id=order_id)
+    if request.method == 'POST':
+        order.status = request.POST.get('status')
+        order.save()
+        return redirect('order_list')
+    return render(request, 'orders/update_order_status.html', {'order': order})
